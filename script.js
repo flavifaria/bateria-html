07dc02e61b9eb0b251cb342b90d3436edfec6fa0
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2025-09-25 10:57:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//Criação das constantes de som
+const sons = {
+    'A' : 'boom.wav',
+    'S' : 'clap.wav',
+    'D' : 'hithat.wav',
+    'F' : 'hithat.wav',
+    'G' : 'kick.wav',
+    'H' : 'openhat.wav',
+    'I' : 'ride.wav',
+    'J' : 'snare.wav',
+    'K' : 'tink.wav',
+    'L' : 'tom.wav',
+}
+//Função para criar uma div para cada tecla
+const criarDiv = (texto) => {
+    div.classList('key');
+    div.textContent = texto;
+}
+
